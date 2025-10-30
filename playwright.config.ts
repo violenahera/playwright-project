a6e8d0dbcc2cbaf7ec17d88842c0896692e03fa7
Violena <violenahera@gmail.com>
2025-10-30 11:58:30 -0500

    Actualizo configuración para CI/CD con headless y GitHub Actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,8 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
+    viewport: { width: 1280, height: 720 },
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
