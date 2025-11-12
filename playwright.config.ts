1122203af3eb6bf98c7416b5eb55350f5950713a
Violena <violenahera@gmail.com>
2025-11-12 18:24:20 -0500

    Actualizacion del modo headless true en el config para que corra el CI/CD

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
